7610801c662a99cd3ae03bfde81cede390b762b4
yancs <467938214@qq.com>
2016-10-08 23:34:31 +0800

    v.0.1.0     总算能看了，这个算有资格是0.10版本了把

--- js/index_ang.js
+++ js/index_ang.js
@@ -38,13 +38,23 @@ for (var i = 0; i < arr.length ; i++) {
     app.controller(arr[i],function($scope,DataService){
         var data = $scope.data = {};
         data.donghua = DataService.donghua_1;
+        data.ranking = DataService.ranking_1;
+        data.ranktop = DataService.ranktop_1;
         var actions = $scope.actions = {};
         actions.newUp = function(flag) {
-
             if (flag == 2) {
                 data.donghua = DataService.donghua_2;
             } else {
                 data.donghua = DataService.donghua_1;
+            }
+        };
+        actions.newRank = function(flag) {
+            if (flag == 2) {
+                data.ranktop = DataService.ranktop_2;
+                data.ranking = DataService.ranking_2;
+            } else {
+                data.ranktop = DataService.ranktop_1;
+                data.ranking = DataService.ranking_1;
             }
         };
     });
@@ -52,12 +62,17 @@ for (var i = 0; i < arr.length ; i++) {
 
 app.factory("DataService",function(){
     var data = {};
-    data.random = Math.floor(4 * Math.random());
+    data.random = Math.floor(9 * Math.random());
     data.active = [
         "./images/active.gif",
         "./images/active2.gif",
         "./images/active3.gif",
-        "./images/active4.gif"
+        "./images/active4.gif",
+        "./images/active5.gif",
+        "./images/active6.gif",
+        "./images/active7.gif",
+        "./images/active8.gif",
+        "./images/active9.gif",
     ];
     data.slider = [{
         "href": "wait build",
@@ -290,5 +305,67 @@ app.factory("DataService",function(){
         "num1": "2724",
         "num2": "46"
     }];
+    data.ranking_1 = [
+        {
+            "ranknum":"2",
+            "title":"【7月】齐木楠雄的灾难(日播版) 69"
+        },
+        {   
+            "ranknum":"3",
+            "title":"【7月】齐木楠雄的灾难(日播版) 70"
+        },
+        {
+            "ranknum":"4",
+            "title":"【10月】我太受欢迎了该怎么办 01"
+        },
+        {
+            "ranknum":"5",
+            "title":"镇魂街 22 武神躯"
+        },
+        {
+            "ranknum":"6",
+            "title":"【10月】无畏魔女 01"
+        },
+        {
+            "ranknum":"7",
+            "title":"【10月】少女编号 01【独家正版】"
+        }  
+    ];
+    data.ranking_2 = [
+        {
+            "ranknum":"2",
+            "title":"【国动万象】傲娇又伟大的异地恋——狐妖小红娘P3"
+        },
+        {   
+            "ranknum":"3",
+            "title":"Because Of You 《小绿和小蓝》手书"
+        },
+        {
+            "ranknum":"4",
+            "title":"【入侵童年】葫芦娃完整版 毁掉经典不眨眼（完结篇）"
+        },
+        {
+            "ranknum":"5",
+            "title":"美队钢铁侠都变成僵尸了？漫威丧尸英雄传！ 第二章【Lorre】"
+        },
+        {
+            "ranknum":"6",
+            "title":"【东拉西扯】在机战动画里寻求福利是否搞错了什么 =="
+        },
+        {
+            "ranknum":"7",
+            "title":"【综漫/燃向AMV】一切皆是因果报应！"
+        } 
+    ];
+    data.ranktop_1 = {
+        "src":"./images/rank1.jpg@320w_200h.webp",
+        "title":"【1月】火影忍者 疾风传 698",
+        "count":"综合评分：78.1万"
+    };
+    data.ranktop_2 = {
+        "src":"./images/rank2.jpg@320w_200h.webp",
+        "title":"[排行向]Cup主最爱的二次元女角排行",
+        "count":"综合评分：18.2万"
+    };
     return data;
 });
